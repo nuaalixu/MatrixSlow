6c235996c14a3bc644be99993fc2056d62c2266d
xu.li <nuaalixu@outlook.com>
2020-11-19 20:55:40 +0800

    add new ops

--- matrixslow/ops/ops.py
+++ matrixslow/ops/ops.py
@@ -102,6 +102,7 @@ class Step(Operator):
         self.value = np.mat(np.where(self.parents[0].value >= 0.0, 1.0, 0.0))
 
     def get_jacobi(self, parent):
+        assert parent is self.parents[0]
         return np.mat(np.zeros((self.dimension, self.dimension)))
 
 
@@ -117,4 +118,180 @@ class logistic(Operator):
             1.0 / (1.0 + np.power(np.e, np.where(-x > 1e2, 1e2, -x))))
 
     def get_jacobi(self, parent):
+        assert parent is self.parents[0]
         return np.diag(np.mat(np.multiply(self.value, 1 - self.value)).A1)
+
+
+class ReLU(Operator):
+    """
+    对矩阵的元素施加ReLU函数
+    """
+
+    nslope = 0.1  # 负半轴的斜率
+
+    def compute(self):
+        self.value = np.mat(np.where(
+            self.parents[0].value > 0.0,
+            self.parents[0].value,
+            self.nslope * self.parents[0].value
+        ))
+
+    def get_jacobi(self, parent):
+        assert parent is self.parents[0]
+        return np.diag(
+            np.where(self.parents[0].value.A1 > 0.0, 1.0, self.nslope))
+
+
+class Reshape(Operator):
+    """
+    改变父节点的值（矩阵）的形状
+    """
+    def __init__(self, *parent, **kargs):
+        super().__init__(*parent, **kargs)
+        self.to_shape = kargs.get('shape')
+        assert isinstance(self.to_shape, tuple) and len(self.to_shape) == 2
+
+    def compute(self):
+        self.value = self.parents[0].value.reshape(self.to_shape)
+
+    def get_jacobi(self, parent):
+        assert parent is self.parents[0]
+        return np.mat(np.eye(self.dimension()))
+
+
+class Multiply(Operator):
+    """
+    求两个父节点的值的element-wise乘积
+    """
+
+    def compute(self):
+        self.value = np.multiply(self.parents[0].value, self.parents[1].value)
+
+    def get_jacobi(self, parent):
+        if parent is self.parents[0]:
+            return np.diag(self.parents[1].value.A1)
+        else:
+            return np.diag(self.parents[0].value.A1)
+
+
+class Convolve(Operator):
+    """
+    以第二个父节点的值为滤波器，对第一个父节点的值做二维离散卷积
+    """
+    def __init__(self, *parents, **kargs):
+        assert len(parents) == 2
+        super().__init__(*parents, **kargs)
+        self.padded = None
+
+    def compute(self):
+        data = self.parents[0].value  # 图像
+        kernel = self.parents[1].value  # 滤波器
+
+        w, h = data.shape  # 图像的宽和高
+        kw, kh = kernel.shape  # 滤波器尺寸
+        hkw, hkh = int(kw / 2), int(kh / 2)  # 滤波器尺寸的一半,为了padding
+
+        # 补齐数据边缘
+        pw, ph = tuple(np.add(data.shape, np.multiply((hkw, hkh), 2)))
+        self.padded = np.mat(np.zeros((pw, ph)))
+        self.padded[hkw:hkw + w, hkh:hkh + h] = data
+        self.value = np.mat(np.zeros(w, h))
+
+        # 二维离线卷积
+        for i in np.arange(hkw, hkw + w):
+            for j in np.arange(hkh, hkh + h):
+                self.value[i - hkw, j - hkh] = np.sum(
+                    np.multiply(
+                        self.padded[i - hkw:i - hkw + kw,
+                                    j - hkh:j - hkh + kh],
+                        kernel)
+                )
+
+    def get_jacobi(self, parent):
+        data = self.parents[0].value
+        kernel = self.parents[1].value
+
+        w, h = data.shape
+        kw, kh = kernel.to_shape
+        hkw, hkh = int(kw / 2), int(kh / 2)
+
+        pw, ph = tuple(np.add(data.shape, np.multiply((hkw, hkh), 2)))
+
+        jacobi = []
+        if parent is self.parents[0]:
+            for i in np.arange(hkw, hkw + w):
+                for j in np.arange(hkh, hkh + h):
+                    mask = np.mat(np.zeros((pw, ph)))
+                    mask[i - hkw:i - hkw + kw, j - hkh:j - hkh + kh] = kernel
+                    jacobi.append(mask[hkw:hkw + w, hkh:hkh + h].A1)
+        elif parent is self.parents[1]:
+            for i in np.arange(hkw, hkw + w):
+                for j in np.arange(hkh, hkh + h):
+                    jacobi.append(
+                        self.padded[i - hkw:i - hkw + kw,
+                                    j - hkh:j - hkh + kh].A1
+                    )
+        else:
+            raise Exception("You aren't my father")
+
+        return np.mat(jacobi)
+
+
+class MaxPooling(Operator):
+    """
+    最大值池化
+    """
+
+    def __init__(self, *parents, **kargs):
+        super().__init__(*parents, **kargs)
+        self.stride = kargs.get('stride')
+        assert self.stride is not None
+        self.stride = tuple(self.stride)
+        assert isinstance(self.stride, tuple) and len(self.stride) == 2
+
+        self.size = kargs.get('size')
+        assert self.size is not None
+        self.size = tuple(self.size)
+        assert isinstance(self.size, tuple) and len(self.size) == 2
+
+        self.flag = None
+
+    def compute(self):
+        data = self.parents[0].value  # 输入特征图
+        w, h = data.shape  # 输入特征图的宽和高
+        dim = w * h
+        sw, sh = self.stride  # 步长
+        kw, kh = self.size  # 池化核尺寸
+        hkw, hkh = int(kw / 2), int(kh / 2)  # 池化核长宽的一半
+
+        result = []
+        flag = []
+
+        for i in np.arange(0, w, sw):
+            row = []
+            for j in np.arange(0, h, sh):
+                # 取池化窗口中的最大值
+                top, bottom = max(0, i - hkw), min(w, i + hkw + 1)
+                left, right = max(0, i - hkh), min(h, j + hkh + 1)
+                window = data[top:bottom, left:right]
+                row.append(np.max(window))
+
+                # 记录最大值在原特征图中的位置
+                pos = np.argmax(window)
+                w_width = right - left
+                # 将池化窗口的坐标转为特征图坐标
+                offset_w, offset_h = top + pos // w_width, left + pos % w_width
+                # 将二维坐标转为flatten一维坐标
+                offset = offset_w * w + offset_h
+                tmp = np.zeros(dim)
+                tmp[offset] = 1
+                flag.append(tmp)
+
+            result.append(row)
+
+        self.flag = np.mat(flag)
+        self.value = np.mat(result)
+
+    def get_jacobi(self, parent):
+        assert parent is self.parents[0] and self.jacobi is not None
+        return self.flag
